6ddbb74c6bdc1b1fd16ca3f19f36a15f8e89597c
john1eng <john1eng@hotmail.com>
2020-10-20 12:06:58 -0400

    Updates

---
+++ precache-manifest.2595f4da2ca1c690298cb6ffff829eaf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1eb630a35692ce82d71d8b98fa77b62",
+    "url": "/games/index.html"
+  },
+  {
+    "revision": "b219b3fb450131aa491d",
+    "url": "/games/static/css/main.fe7f138d.chunk.css"
+  },
+  {
+    "revision": "184867184eba765ec485",
+    "url": "/games/static/js/2.2629b4ee.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/games/static/js/2.2629b4ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b219b3fb450131aa491d",
+    "url": "/games/static/js/main.4d107527.chunk.js"
+  },
+  {
+    "revision": "5d4186a616588cdd3c40",
+    "url": "/games/static/js/runtime-main.cf35df89.js"
+  }
+]);
